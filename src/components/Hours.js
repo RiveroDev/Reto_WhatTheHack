2f842cbeb0a9e06d1f631f41a63287abb791c5c8
Richard Rivero <Richard.Rivero.dev@gmail.com>
2024-08-28 18:10:53 +0000

    new component Hours

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,29 @@
 // Placeholder to host a component
+import React, { useState, useEffect } from 'react';
+
+function Hours() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const   
+ hours = currentTime.getHours();
+  const minutes = currentTime.getMinutes();
+  const seconds = currentTime.getSeconds();
+
+  return   
+ (
+    <div>
+      <h2>Hora actual:</h2>
+      <p>{hours}:{minutes}:{seconds}</p>
+    </div>
+  );
+}
+
+export default Hours;
